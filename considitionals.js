fa24f85808461586f7cb3def9a765556dfe7c5f7
Rahul <rahul.shial@gmail.com>
2020-09-30 20:02:54 +0000

    add more conditional examples

--- considitionals.js
+++ considitionals.js
@@ -274,7 +274,7 @@ if (cold) {
 
 console.log("Now you're ready to go outside!");
 
-const temperature = 12;
+const temperature = 52;
 
 if (temperature < 0) {
   console.log("Make sure you pick out a scarf!");
@@ -286,3 +286,18 @@ if (temperature < 0) {
 
 console.log("Now you're ready to go outside!");
 
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+if (temperature < -40 || temperature > 40) {
+    console.log("Maybe going outside isn't such a great idea…");
+}
+
+if (!raining) {
+    console.log("Leave your umbrella at home!");
+  }
+  
