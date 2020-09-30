38673ea7b5d3ba7ea51ef188dfd9b2c5c5bcacbd
Rahul <rahul.shial@gmail.com>
2020-09-30 19:44:00 +0000

    committing the initial section 4 exercises.

---
+++ tinkering.js
@@ -0,0 +1,71 @@
+function multiply(a, b) {
+    return a * b;
+}
+
+let result = multiply(2, 4);
+console.log(result);
+
+result = multiply(-2, 3);
+console.log(result);
+
+console.log ("2 + 10 - 19 + 4 - 90 +  = " + (2 + 10 - 19 + 4 - 90 + 1));
+console.log("-20 + -19 - (-10) - (-1) + 24 = " + (-20 + -19 - (-10) - (-1) + 24));
+console.log("(10/5) * 4 - 20 = " + ((10/5) * 4 - 20));
+console.log("4096 % 12 = " + (4096 % 12));
+console.log(2 * 40 - 40 + 2)
+console.log("Hello" + 5*10);
+
+var celsius = 12;
+var fahrenheit = celsius * 1.8 + 32;
+console.log(fahrenheit);
+
+var quote = "Stay awhile and listen!";
+console.log(quote[6]);
+
+// Pick a string. Your string can have any number of characters.
+var my_string = "a";
+
+// Calculate the ASCII value of the first character, i.e. the character at the position 0. 
+var ASCII_value = my_string.charCodeAt(0);
+
+// Let us print
+console.log(ASCII_value);
+
+// var answer = "All STRINGS are CrEaTED Equal" == "All STRINGS are CrEaTED Equal";
+// console.log(answer);
+
+// console.log("Why couldn\'t the shoes go out and play?\nThey were all \"tied\" up!");
+
+// var haiku = "Blowing from the west"+"\nFallen leaves gather"+"\nIn the east."
+// console.log(haiku);
+
+var x = null;
+var y;
+console.log(x, y);
+
+if ("false"===0) {
+    console.log("True");
+} else {
+    console.log("false");
+}
+
+var thingOne = "red";
+var thingTwo = "blue";
+console.log(thingOne + " " + thingTwo);
+var bill = 10.25 + 3.99 + 7.15;
+var tip = bill * 0.15;
+var total = bill + tip;
+console.log("$" + total);
+
+var firstName = "Julia";
+var interest = "cats";
+var hobby = "play video games";
+var awesomeMessage = "Hi, my name is " + firstName + ". I love " + interest + ". In my spare time, I like to " + hobby + ".";
+console.log(awesomeMessage);
+
+let ten = 10;
+console.log(ten + 3);
+console.log(ten * 3);
+console.log(ten / 3);
+console.log(ten % 3);
+
