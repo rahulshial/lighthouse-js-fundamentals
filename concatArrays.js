c2615bbcc0c275709eb811b4952a43056fc106e2
Rahul <rahul.shial@gmail.com>
2020-10-01 19:33:49 +0000

    concat arrays - merge

--- concatArrays.js
+++ concatArrays.js
@@ -3,10 +3,18 @@ const concat = function (array1, array2) {
   return concatArray;
 }
 
-console.log(concat([1, 2, 3], [4, 5, 6]), "=?", [1, 2, 3, 4, 5, 6]);
-console.log(concat([0, 3, 1], [9, 7, 2]), "=?", [0, 3, 1, 9, 7, 2]);
-console.log(concat([], [9, 7, 2]), "=?", [9, 7, 2]);
-console.log(concat([5, 10], []), "=?", [5, 10]);
+const merge = function( array1, array2) {
+  let concatArray = array1.concat(array2);
+  concatArray.sort();
+  return concatArray;
+}
+
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
+
+// console.log(concat([1, 2, 3], [4, 5, 6]), "=?", [1, 2, 3, 4, 5, 6]);
+// console.log(concat([0, 3, 1], [9, 7, 2]), "=?", [0, 3, 1, 9, 7, 2]);
+// console.log(concat([], [9, 7, 2]), "=?", [9, 7, 2]);
+// console.log(concat([5, 10], []), "=?", [5, 10]);
 
 // var hege = ["Cecilie", "Lone"];
 // var stale = ["Emil", "Tobias", "Linus"];
