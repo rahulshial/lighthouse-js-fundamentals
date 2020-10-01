be5a192fb37c443c513dacc030436ebf5d4cf957
Rahul <rahul.shial@gmail.com>
2020-10-01 16:06:39 +0000

    arrays

---
+++ arrays.js
@@ -0,0 +1,9 @@
+const director = "Steven Spielberg";
+const films = ["Jaws", "Raiders of the Lost Ark", "A.I."];
+console.log(films);
+films[2] = "Jurassic Park";
+console.log(films);
+films.push("A.I.");
+console.log(films);
+films.push("E.T.");
+console.log(films);
