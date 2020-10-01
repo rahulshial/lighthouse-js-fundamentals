6260a08bbba9e77c342a96333d6e5e25c23fcf19
Rahul <rahul.shial@gmail.com>
2020-10-01 23:03:39 +0000

    smart garbage

--- smartGarbage.js
+++ smartGarbage.js
@@ -1,7 +1,16 @@
-
+const bins = {
+  waste: 0,
+  recycling: 0,
+  compost: 0
+};
 
 
 const smartGarbage = function (trash, bins) {
-  // Your code in here ...
+  bins[trash] += 1;
+  console.log(bins);
+  return bins;
 }
 
+smartGarbage('recycling', { waste: 4, recycling: 2, compost: 5 });
+
+console.log(bins);
