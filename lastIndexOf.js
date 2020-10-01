4e0edcfae35199caa5e2561624fc680c825597d6
Rahul <rahul.shial@gmail.com>
2020-10-01 18:21:27 +0000

    last of index

---
+++ lastIndexOf.js
@@ -0,0 +1,20 @@
+
+const lastIndexOf = function (array, searchValue) {
+  if (array.length === 0) {
+    return -1;
+  } else {
+    for (let i = array.length - 1; i >= 0; i--) {
+      if (array[i] === searchValue) {
+        return i;
+      } else if (i === 0) {
+        return -1;
+      }
+    }
+  }
+}
+
+console.log(lastIndexOf([0, 1, 4, 1, 2], 1), "=?", 3);
+console.log(lastIndexOf([0, 1, 4, 1, 2], 2), "=?", 4);
+console.log(lastIndexOf([0, 1, 4, 1, 2], 3), "=?", -1);
+console.log(lastIndexOf([5, 5, 5], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
